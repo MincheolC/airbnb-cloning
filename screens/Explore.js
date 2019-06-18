fb951f6107b87136087b19041ec13d6a05ee54fc
MincheolC <mincheol@encoretech.com>
2019-06-18 16:27:21 +0900

    feat: horizontal scrollView

--- screens/Explore.js
+++ screens/Explore.js
@@ -2,18 +2,25 @@ import React, { Component } from "react";
 import {
   SafeAreaView,
   StyleSheet,
+  ScrollView,
   View,
+  Text,
   TextInput,
+  Image,
   Platform,
-  StatusBar
+  StatusBar,
+  Dimensions
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import Category from "./components/Explore/Category";
+
+const { height, width } = Dimensions.get("window");
 
 class Explore extends Component {
   componentWillMount() {
     this.startHeaderHeight = 80;
-    if (Platform.OS == 'android') {
-      this.startHeaderHeight = 100 + StatusBar.currentHeight
+    if (Platform.OS == "android") {
+      this.startHeaderHeight = 100 + StatusBar.currentHeight;
     }
   }
 
@@ -42,15 +49,74 @@ class Explore extends Component {
                 marginTop: Platform.OS == "android" ? 30 : null
               }}
             >
-              <Ionicons name="ios-search" size={20} style={{ marginRight: 10 }}/>
+              <Ionicons
+                name="ios-search"
+                size={20}
+                style={{ marginRight: 10 }}
+              />
               <TextInput
                 underlineColorAndroid="transparent"
                 placeholder="Try New Delhi"
                 placeholderTextColor="grey"
-                style={{ flex: 1, fontWeight: '700', backgroundColor: 'white' }}
+                style={{ flex: 1, fontWeight: "700", backgroundColor: "white" }}
               />
             </View>
           </View>
+          <ScrollView scrollEventThrottle={16}>
+            <View style={{ flex: 1, backgroundColor: "white", paddingTop: 20 }}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: "700",
+                  paddingHorizontal: 20
+                }}
+              >
+                What can we help you find, Mincheol??
+              </Text>
+
+              <View style={{ height: 130, marginTop: 20 }}>
+                <ScrollView
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                >
+                  <Category
+                    imageUri={require("../assets/home.jpg")}
+                    name="Home"
+                  />
+                  <Category
+                    imageUri={require("../assets/experiences.jpg")}
+                    name="Experiences"
+                  />
+                  <Category
+                    imageUri={require("../assets/restaurant.jpg")}
+                    name="Restaurant"
+                  />
+                </ScrollView>
+              </View>
+              <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+                <Text style={{ fontSize: 24, fontWeight: "700" }}>
+                  Introducing Airbnb Plus
+                </Text>
+                <Text style={{ fontWeight: "100", marginTop: 10 }}>
+                  A new selection of homes verified for quality & comfort
+                </Text>
+                <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
+                  <Image
+                    style={{
+                      flex: 1,
+                      height: null,
+                      width: null,
+                      resizeMode: "cover",
+                      borderRadius: 5,
+                      borderWidth: 1,
+                      borderColor: "#dddddd"
+                    }}
+                    source={require("../assets/home.jpg")}
+                  />
+                </View>
+              </View>
+            </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     );
